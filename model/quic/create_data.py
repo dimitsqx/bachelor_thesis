cd3d7e7077f46a8732575ae2f4d14300a4042f0a
dimitsqx <dimitriueu@gmail.com>
2021-03-08 16:38:20 +0000

    new model for MNIST

--- model/quic/create_data.py
+++ model/quic/create_data.py
@@ -4,33 +4,67 @@ import numpy as np
 import tensorflow as tf
 import tensorflow_federated as tff
 import pickle
+from matplotlib import pyplot as plt
+from fl_model import create_keras_model
 
 emnist_train, emnist_test = tff.simulation.datasets.emnist.load_data()
 
 
 def preprocess(dataset):
-
+    '''
+    Preprocess for CNN
+    '''
     def batch_format_fn(element):
-        """Flatten a batch `pixels` and return the features as an `OrderedDict`."""
-        return (tf.reshape(element['pixels'], [-1, 784]), tf.reshape(element['label'], [-1, 1]))
+        return (tf.reshape(element['pixels'], [28, 28, 1]), element['label'])
 
     def generate_array():
         for element in dataset.map(batch_format_fn):
-            yield element[0][0], element[1][0][0]
+            yield element[0], element[1]
     x, y = zip(*generate_array())
     return np.array(x), np.array(y)
 
 
-data = dict()
-i = 0
+model = create_keras_model()
 for client_id in emnist_train.client_ids:
-    print(client_id, '       ', i)
+    print(client_id)
     x_train, y_train = preprocess(emnist_train.create_tf_dataset_for_client(
         client_id))
     y_train = tf.keras.utils.to_categorical(y_train)
-    data[client_id] = (x_train, y_train)
-    i += 1
+    model.fit(x_train, y_train, epochs=5, batch_size=40, validation_split=0.2)
 
-with open('EMNINST.pickle', 'wb') as fp:
-    print('writing data')
-    pickle.dump(data, fp)
+for client_id in emnist_test.client_ids:
+    print(client_id)
+    x_train, y_train = preprocess(emnist_train.create_tf_dataset_for_client(
+        client_id))
+    y_train = tf.keras.utils.to_categorical(y_train)
+    score = model.evaluate(x_train, y_train, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+
+# data = dict()
+# i = 0
+# for client_id in emnist_train.client_ids:
+#     print(client_id, '       ', i)
+#     x_train, y_train = preprocess(emnist_train.create_tf_dataset_for_client(
+#         client_id))
+#     y_train = tf.keras.utils.to_categorical(y_train)
+#     data[client_id] = (x_train, y_train)
+#     i += 1
+
+# with open('EMNINST_TRAIN.pickle', 'wb') as fp:
+#     print('writing train  data')
+#     pickle.dump(data, fp)
+
+# data = dict()
+# i = 0
+# for client_id in emnist_test.client_ids:
+#     print(client_id, '       ', i)
+#     x_train, y_train = preprocess(emnist_train.create_tf_dataset_for_client(
+#         client_id))
+#     y_train = tf.keras.utils.to_categorical(y_train)
+#     data[client_id] = (x_train, y_train)
+#     i += 1
+
+# with open('EMNINST_TEST.pickle', 'wb') as fp:
+#     print('writing test data')
+#     pickle.dump(data, fp)
--- model/quic/fl_model.py
+++ model/quic/fl_model.py
@@ -7,11 +7,42 @@ import pickle
 
 
 def create_keras_model():
-    network = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(512, activation='relu', input_shape=(28 * 28,)),
-        tf.keras.layers.Dense(10, activation='softmax'),
-    ])
-    network.compile(optimizer='rmsprop',
-                    loss='categorical_crossentropy',
-                    metrics=['accuracy'])
-    return network
+    # model building
+    model = tf.keras.models.Sequential()
+    # convolutional layer with rectified linear unit activation
+    model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3),
+                                     activation='relu',
+                                     input_shape=(28, 28, 1)))
+    # 32 convolution filters used each of size 3x3
+    # again
+    # model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+    # 64 convolution filters used each of size 3x3
+    # choose the best features via pooling
+    model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+    # randomly turn neurons on and off to improve convergence
+    # flatten since too many dimensions, we only want a classification output
+    model.add(tf.keras.layers.Flatten())
+    # fully connected to get all relevant data
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    # output a softmax to squash the matrix into output probabilities
+    model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+    model.compile(loss=tf.keras.losses.categorical_crossentropy,
+                  optimizer=tf.keras.optimizers.Adadelta(),
+                  metrics=['accuracy'])
+    return model
+
+
+# (trainX, trainY), (testX, testY) = tf.keras.datasets.mnist.load_data()
+# # reshape dataset to have a single channel
+# trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
+# testX = testX.reshape((testX.shape[0], 28, 28, 1))
+# # one hot encode target values
+# trainY = tf.keras.utils.to_categorical(trainY)
+# testY = tf.keras.utils.to_categorical(testY)
+
+# trainX = trainX.astype('float32')
+# testX = testX.astype('float32')
+# # normalize to range 0-1
+# trainX = trainX / 255.0
+# testX = testX / 255.0
